e3a3c4637d4f9e1faeffa2e26376b2bbaebfa0a5
wli-dev7 <henry@weblineindia.com>
2020-09-12 14:58:41 +0530

    New PWA Build with caching of API data

---
+++ app/Live Build/precache-manifest.ce59ab33de7b3a2395fb40d7d3783a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b7c2c4f205534378e71116f3152548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a15c97254e7c08b508f",
+    "url": "/static/css/main.9db3f151.chunk.css"
+  },
+  {
+    "revision": "107152959ab276634178",
+    "url": "/static/js/2.ec74c8c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ec74c8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a15c97254e7c08b508f",
+    "url": "/static/js/main.aa58e4ab.chunk.js"
+  },
+  {
+    "revision": "2a1adb1b73c95a609e9e",
+    "url": "/static/js/runtime-main.183cdd73.js"
+  }
+]);
