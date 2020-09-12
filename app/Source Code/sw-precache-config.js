6ab7890a355b73fe4c44138ca490c76b305b1cb5
wli-dev7 <henry@weblineindia.com>
2020-09-12 15:12:49 +0530

    NEW : Code changes related to caching of API data when app is offline

---
+++ app/Source Code/sw-precache-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    stripPrefix: 'build/',
+    staticFileGlobs: [
+      'build/*.html',
+      'build/manifest.json',
+      'build/static/**/!(*map*)',
+  
+    ],
+    dontCacheBustUrlsMatching: /\.\w{8}\./,
+    runtimeCaching: [{
+      urlPattern: /api/,
+      handler: 'networkFirst'
+    }],
+    swFilePath: 'build/service-worker.js'
+  };
+  
